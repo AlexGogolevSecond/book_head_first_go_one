14e8ad98b6cc87207299f9639f7c32fe1968efca
Aleksandr <a.gogolev@carbonsoft.ru>
2025-04-13 22:58:08 +0500

    if != nil

--- utils/pass_fail.go
+++ utils/pass_fail.go
@@ -5,12 +5,17 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"log"
 )
 
 func PassFail() {
 	fmt.Print("Enter a grade: ")
 	reader := bufio.NewReader(os.Stdin) // Создать «буферизованное средство чтения» для получения текста с клавиатуры
-	input, _ := reader.ReadString('\n') //Возвращает текст, введенный пользователем до нажатия клавиши Enter
+	input, err := reader.ReadString('\n') //Возвращает текст, введенный пользователем до нажатия клавиши Enter
+	if err != nil {
+		log.Fatal(err)	
+	}
+	
 	fmt.Println(input)
 }
 
