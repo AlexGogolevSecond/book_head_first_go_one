ca6092331197ed2cf76fe80957ebe257358fa50b
Aleksandr <a.gogolev@carbonsoft.ru>
2025-04-16 14:42:02 +0500

    циклы + преобразование числа в строку

--- utils/guess.go
+++ utils/guess.go
@@ -19,7 +19,7 @@ func Guess() {
 	// fmt.Printf("rnd %d\n", rnd)
 	fmt.Println("I've chosen a random number between 1 and 100.")
 	fmt.Println("Can you guess it?")
-	  fmt.Println(rnd)
+	fmt.Println(rnd)
 	reader := bufio.NewReader(os.Stdin) // Создать «буферизованное средство чтения» для получения текста с клавиатуры
 	fmt.Print("Your guess: ")
 	input, err := reader.ReadString('\n')  // читаем данные из консоли
@@ -31,6 +31,15 @@ func Guess() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	
+	for x:=1; x < 5; x++ {
+		y := x + 10
+		fmt.Println("y" + strconv.Itoa(y))  // !!! пробразуем int в строку
+	}
+
+	// for x:=10; x > 0; x-=2 {
+	// 	fmt.Println(x)
+	// }
 	
 	if guess < rnd {
 		fmt.Println("Oops. Your guess was LOW")
