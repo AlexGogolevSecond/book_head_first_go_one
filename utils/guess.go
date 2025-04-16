5852c9f708d71cf0848ec97555660cb37bf2ff49
Aleksandr <a.gogolev@carbonsoft.ru>
2025-04-16 09:57:41 +0500

    added some text

--- utils/guess.go
+++ utils/guess.go
@@ -12,5 +12,7 @@ func Guess() {
 	//rnd := rand.Intn(100) + 1  // интересная реализация - тут когда +1, то диапазон от 1 до 100, иначе от 0 до 99
 	rnd := rand.Intn(100) + 1
 	// fmt.Printf("rnd %d\n", rnd)
+	fmt.Println("I've chosen a random number between 1 and 100.")
+	fmt.Println("Can you guess it?")
 	fmt.Println(rnd)
 }
