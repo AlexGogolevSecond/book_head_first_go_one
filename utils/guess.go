b48d1a777af6c232f216b6e5a925dec3ad997dfa
Aleksandr <a.gogolev@carbonsoft.ru>
2025-04-16 09:04:21 +0500

    test

--- utils/guess.go
+++ utils/guess.go
@@ -8,7 +8,7 @@ import (
 func Guess() {
 	// // работает и без time, но в книге используется
 	// seconds := time.Now().Unix()
-	// rand.Seed(seconds)
+	// rand.Seed(seconds)  //lalala
 	//rnd := rand.Intn(100) + 1  // интересная реализация - тут когда +1, то диапазон от 1 до 100, иначе от 0 до 99
 	rnd := rand.Intn(100) + 1
 	// fmt.Printf("rnd %d\n", rnd)
