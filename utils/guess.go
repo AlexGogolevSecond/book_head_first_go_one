b6eb5fb1cce1fe1aea6b22f3a68c7af0e384f96f
Aleksandr <a.gogolev@carbonsoft.ru>
2025-04-17 09:13:32 +0500

    циклы

--- utils/guess.go
+++ utils/guess.go
@@ -21,29 +21,37 @@ func Guess() {
 	fmt.Println("Can you guess it?")
 	fmt.Println(rnd)
 	reader := bufio.NewReader(os.Stdin) // Создать «буферизованное средство чтения» для получения текста с клавиатуры
-	fmt.Print("Your guess: ")
-	input, err := reader.ReadString('\n')  // читаем данные из консоли
-	if err != nil {
-		log.Fatal(err)
-	}
-	input = strings.TrimSpace(input)  // удаляем символы справа/слева
-	guess, err := strconv.Atoi(input)  // преобразуем строку в int
-	if err != nil {
-		log.Fatal(err)
-	}
-	
-	for x:=1; x < 5; x++ {
-		y := x + 10
-		fmt.Println("y" + strconv.Itoa(y))  // !!! пробразуем int в строку
-	}
-	// fmt.Print(x) //x недоступен в текущей области видимости !!!
-	// for x:=10; x > 0; x-=2 {
-	// 	fmt.Println(x)
-	// }
-	
-	if guess < rnd {
-		fmt.Println("Oops. Your guess was LOW")
-	} else if guess > rnd {
-		fmt.Println("Oops. Your guess was HIGH")
+	for guesses := 0; guesses < 10; guesses++ {
+		fmt.Println("You have", 10-guesses, "guesses left.")
+
+		fmt.Print("Your guess: ")
+		input, err := reader.ReadString('\n')  // читаем данные из консоли
+		if err != nil {
+			log.Fatal(err)
+		}
+		input = strings.TrimSpace(input)  // удаляем символы справа/слева
+		guess, err := strconv.Atoi(input)  // преобразуем строку в int
+		if err != nil {
+			log.Fatal(err)
+		}
+		
+		// for x:=1; x < 5; x++ {
+		// 	y := x + 10
+		// 	fmt.Println("y" + strconv.Itoa(y))  // !!! пробразуем int в строку
+		
+		// fmt.Print(x) //x недоступен в текущей области видимости !!!
+		// for x:=10; x > 0; x-=2 {
+		// 	fmt.Println(x)
+		// }
+		
+		if guess < rnd {
+			fmt.Println("Oops. Your guess was LOW")
+		} else if guess > rnd {
+			fmt.Println("Oops. Your guess was HIGH")
+		} else {
+			fmt.Println("You are win!!! You spent " + strconv.Itoa(guesses + 1) + " attempt")
+			break
+		}
 	}
 }
+
